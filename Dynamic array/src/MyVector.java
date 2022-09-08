08553135617c4293cb6ba944298fafde21bcc135
ElitsaY <eyotkova@gmail.com>
2022-09-08 17:42:14 +0300

    Adding class variables. Initial commit

---
+++ Dynamic array/src/MyVector.java
@@ -0,0 +1,7 @@
+public class MyVector {
+    private final static int GROWTH_COEFICIENT = 2;
+    private final static int INITIAL_CAPACITY = 8;
+
+    private int capacity;
+    private int size;
+}
