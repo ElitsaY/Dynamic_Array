39fe9662983b293598d39d7ad5c497cdaf71652d
ElitsaY <eyotkova@gmail.com>
2022-09-11 17:51:07 +0300

    Implemented constructors

--- Dynamic array/src/MyVector.java
+++ Dynamic array/src/MyVector.java
@@ -1,7 +1,36 @@
+import java.util.Arrays;
+
 public class MyVector {
-    private final static int GROWTH_COEFICIENT = 2;
+    private final static int GROWTH_COEFFICIENT = 2;
     private final static int INITIAL_CAPACITY = 8;
 
     private int capacity;
     private int size;
+
+    private int[] container;
+
+    //constructors
+    MyVector(){
+        container = new int[INITIAL_CAPACITY];
+        size = 0;
+        capacity = INITIAL_CAPACITY;
+    }
+
+    MyVector(int newSize){
+        container = new int[newSize];
+        size = newSize;
+        capacity = newSize;
+    }
+
+    MyVector(int[] array){
+        container = new int[array.length];
+        container = Arrays.copyOf(array, array.length);
+        size = array.length;
+        capacity = array.length;
+    }
+    //memory related functions
+
+    //dynamic array operations
+
+    //interaction with other arrays
 }
