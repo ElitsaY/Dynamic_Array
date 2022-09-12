e1ab8561be76cab4593b0a365edb2f5de7dab279
ElitsaY <eyotkova@gmail.com>
2022-09-12 19:41:08 +0300

    Implemented constructors

--- DynamicArray/src/DynamicArray.java
+++ DynamicArray/src/DynamicArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DynamicArray<E>{
      private final static int INITIAL_CAPACITY = 8;
      private final static int GROWTH_COEFFICIENT = 2;
@@ -6,5 +8,31 @@ public class DynamicArray<E>{
      private int capacity;
 
      private Object[] container;
-     
+
+     //Constructors
+     DynamicArray(){
+          container = new Object[INITIAL_CAPACITY];
+          size = 0;
+          capacity = INITIAL_CAPACITY;
+     }
+
+     DynamicArray(int newSize) {
+          container = new Object[newSize];
+          size = newSize;
+          capacity = newSize;
+     }
+
+     DynamicArray(Object[] newContainer){
+          container = Arrays.copyOf(newContainer, newContainer.length);
+          size = newContainer.length;
+          capacity = newContainer.length;;
+     }
+
+     //getters and setters
+
+     //memory related functions
+
+     //dynamic array operations
+
+     //interacting with other array methods
 }
