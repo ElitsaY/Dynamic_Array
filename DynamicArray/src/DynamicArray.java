03c2c32c4c0db85b49b32b47ddd4140303a1b659
ElitsaY <eyotkova@gmail.com>
2022-09-12 19:09:44 +0300

    Initial commit. Declared class variables

---
+++ DynamicArray/src/DynamicArray.java
@@ -0,0 +1,10 @@
+public class DynamicArray<E>{
+     private final static int INITIAL_CAPACITY = 8;
+     private final static int GROWTH_COEFFICIENT = 2;
+
+     private int size;
+     private int capacity;
+
+     private Object[] container;
+     
+}
